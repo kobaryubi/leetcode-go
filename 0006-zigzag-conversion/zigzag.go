140a5ddaf497655f45453f76bb3ce2cfdfc8c3c1
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-05-20 06:10:50 +0900

    Add zigzag conversion function skeleton

---
+++ 0006-zigzag-conversion/zigzag.go
@@ -0,0 +1,9 @@
+package zigzag
+
+// convert returns s read off in a zigzag pattern over numRows rows.
+//
+// https://leetcode.com/problems/zigzag-conversion/
+func convert(s string, numRows int) string {
+	// TODO: implement
+	panic("not implemented")
+}
