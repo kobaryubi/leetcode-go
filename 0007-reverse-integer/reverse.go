a119e86e1a2561eb6fe4611a98f540b31adaa0c6
Masahiko Kobayashi <kobaryubi6@gmail.com>
2026-05-27 06:29:33 +0900

    Add reverse integer function skeleton
    Stub reverse(x int) int returning 0 to scaffold LeetCode problem 7.

---
+++ 0007-reverse-integer/reverse.go
@@ -0,0 +1,9 @@
+package reverse
+
+// reverse returns x with its digits reversed. If the reversed value falls
+// outside the signed 32-bit integer range, it returns 0.
+//
+// https://leetcode.com/problems/reverse-integer/
+func reverse(x int) int {
+	return 0
+}
